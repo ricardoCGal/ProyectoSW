ab7555a6c8ba9142dd00b9a569b1d37be6408c48
RicardoCG <cortesgalindor@gmail.com>
2023-11-05 15:54:08 -0600

    Empezamso de nuevo

--- principal.php
+++ principal.php
@@ -31,7 +31,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#" >
-                <img src="img/control1.png" alt="Logo" width="70" height="60" class="d-inline-block align-text-top">
+                <img src="img/control1.png" alt="Logo" class="d-inline-block align-text-top">
             </a>
             <a href="principal.php" id="tit"> El Rincon Gamer</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,16 +41,16 @@
             <ul class="navbar-nav justify-content-center" >
               <li class="nav-item">
                 <a class="btn btn-dark" aria-current="page" href="principal.php">Inicio  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
-                  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
-                </svg>
+                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
+                  </svg>
                 </a>
               </li>
               <li id="salir" class="nav-item" >
                 <a href="logout.php" class="btn btn-dark" onclick="confirmarsalir();">Salir 
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
-                    <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
-                    <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"/>
-                  </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
+                <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
+                <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0z"/>
+              </svg>
                 </a>
               </li>
             </ul>
@@ -66,26 +66,9 @@
         
 
     <div class="contenedor-grid">
-      <!--<div class="class-elem1">
       
-      </div>-->
-      <div class="inicio">
-        <p>Bienvenido "<?= $usuarios['user']; ?>"</p>
-      </div>  
-      <div class="compartido">
-        <div class="aside"><!--Aside-->
-          <ul>
-            <li>Publicar</li>
-          </ul>
-        </div>  
-        <div class="container"><!--Contenido-->
-          <p>Contenido</p>
-        </div>
-      </div>  
-      <div class="footer"><!--Footer-->
-        <p>Footer  </p>
-      </div>  
     </div>
+    
 
 
 
